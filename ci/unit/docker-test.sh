16301b119b84626c0dd93d150862b3b73744f6e8
Jake Landis <jake.landis@elastic.co>
2018-05-30 15:32:01 -0500

    add support to test against SNAPSHOTs

--- ci/unit/docker-test.sh
+++ ci/unit/docker-test.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# This is intended to be run the plugin's root directory.
-# To run manually, ensure you have Docker installed and set the ELASTIC_STACK_VERSION environment variable.
+# This is intended to be run the plugin's root directory. `ci/unit/docker-test.sh`
+# Ensure you have Docker installed locally and set the ELASTIC_STACK_VERSION environment variable.
 set -e
 
-whoami
 if [ "$ELASTIC_STACK_VERSION" ]; then
     echo "Testing against version: $ELASTIC_STACK_VERSION"
+
+    if [[ "$ELASTIC_STACK_VERSION" = *"-SNAPSHOT" ]]; then
+        cd /tmp
+        wget https://snapshots.elastic.co/docker/logstash-"$ELASTIC_STACK_VERSION".tar.gz
+        tar xfvz logstash-"$ELASTIC_STACK_VERSION".tar.gz  repositories
+        echo "Loading docker image: "
+        cat repositories
+        docker load < logstash-"$ELASTIC_STACK_VERSION".tar.gz
+        rm logstash-"$ELASTIC_STACK_VERSION".tar.gz
+        cd -
+    fi
+
     if [ -f Gemfile.lock ]; then
         rm Gemfile.lock
     fi
+
     docker-compose -f ci/unit/docker-compose.yml down
     docker-compose -f ci/unit/docker-compose.yml up --build --exit-code-from logstash --force-recreate
 else
